cc663f3d13e54a6ec9b56dc24e7e45d5937ecc12
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-03-04 19:35:43 +0300

    added simplified interface

--- main.cpp
+++ main.cpp
@@ -1,2 +1,37 @@
+#include <iostream>
+
+template< class T >
+struct BiList {
+    T val;
+    BiList< T > * next;
+    BiList< T > * prev;
+};
+
+template< class T >
+BiList< T > * add(BiList< T > * h, const T & d);
+
+template< class T >
+BiList< T > * insert(BiList< T > * h, const T & d);
+
+template< class T >
+BiList< T > * cut(BiList< T > * h) noexcept;
+
+template< class T >
+BiList< T > * eraseAfter(BiList< T > * h) noexcept;
+
+template< class T >
+BiList< T > * eraseBefore(BiList< T > * h) noexcept;
+
+template< class T >
+BiList< T > * clear(BiList< T > * h, BiList< T > * e) noexcept;
+
+template< class T, class F >
+F traverse(F f, BiList< T > * h, BiList< T > * e);
+
+template< class T, class F >
+F traverseRevers(F f, BiList< T > * h, BiList< T > * e);
+
 int main()
-{}
+{
+    std::cout << "\n";
+}
