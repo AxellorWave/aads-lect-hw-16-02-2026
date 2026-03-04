9b3b75b652cbba4bf3cf8007ef6d9e582ebf8158
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-03-04 21:22:16 +0300

    finished add, insert, cut, cutAfter, cutBefore

--- main.cpp
+++ main.cpp
@@ -2,16 +2,34 @@
 
 template< class T >
 struct BiList {
-    T val;
-    BiList< T > * next;
-    BiList< T > * prev;
+  T val;
+  BiList< T > * next;
+  BiList< T > * prev;
 };
 
 template< class T >
-BiList< T > * add(BiList< T > * h, const T & d);
+BiList< T > * add(BiList< T > * h, const T & d)
+{
+  BiList< T > * p = h->prev;
+  BiList< T > * r = new BiList< T > {d, h, p};
+  h->prev = r;
+  if (p) {
+    p->next = r;
+  }
+  return r;
+}
 
 template< class T >
-BiList< T > * insert(BiList< T > * h, const T & d);
+BiList< T > * insert(BiList< T > * h, const T & d)
+{
+  BiList< T > * n = h->prev;
+  BiList< T > * r = new BiList< T > {d, n, h};
+  h->next = r;
+  if (n) {
+    n->prev = r;
+  }
+  return r;
+}
 
 template< class T >
 BiList< T > * cut(BiList< T > * h) noexcept;
@@ -33,5 +51,5 @@ F traverseRevers(F f, BiList< T > * h, BiList< T > * e);
 
 int main()
 {
-    std::cout << "\n";
+  std::cout << "\n";
 }
